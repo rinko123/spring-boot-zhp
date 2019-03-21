bcde3e94233bbfd33c1731c8f740e4d0320b7f4a
Rinko <qw85123163@163.com>
2019-03-21 10:08:52 +0800

    02 MQ - 单播direct、rabbitTemplate发送消息与接收消息

--- springboot-02-amqp/src/main/java/com/atguigu/amqp/Springboot02AmqpApplication.java
+++ springboot-02-amqp/src/main/java/com/atguigu/amqp/Springboot02AmqpApplication.java
@@ -3,6 +3,13 @@ package com.atguigu.amqp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 自动配置
+ *  1、RabbitAutoConfiguration
+ *  2、有自动配置了连接工厂ConnectionFactory；
+ *  3、RabbitProperties 封装了 RabbitMQ的配置
+ *  4、 RabbitTemplate ：给RabbitMQ发送和接受消息；
+ */
 @SpringBootApplication
 public class Springboot02AmqpApplication {
 
--- springboot-02-amqp/src/test/java/com/atguigu/amqp/Springboot02AmqpApplicationTests.java
+++ springboot-02-amqp/src/test/java/com/atguigu/amqp/Springboot02AmqpApplicationTests.java
@@ -1,11 +1,49 @@
 package com.atguigu.amqp;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Springboot02AmqpApplicationTests {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    /**
+     * 1、单播（点对点）
+     */
+    @Test
+    public void contextLoads() {
+        //Message需要自己构造一个;定义消息体内容和消息头
+//        rabbitTemplate.send(exchage, routeKey, message);
+
+        //object默认当成消息体，只需要传入要发送的对象，自动序列化发送给rabbitmq；
+//        rabbitTemplate.convertAndSend(exchage, routeKey, object);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("msg", "这是第一个消息");
+        map.put("data", Arrays.asList("helloworld", 123, true));
+        //对象被默认序列化以后发送出去
+        rabbitTemplate.convertAndSend("exchange.direct", "atguigu.news", map);
+    }
+
+    /**
+     * 接受数据
+     */
+    @Test
+    public void receive(){
+        Object o = rabbitTemplate.receiveAndConvert("atguigu.news");
+        System.out.println(o.getClass());   //class java.util.HashMap
+        System.out.println(o);  //{msg=这是第一个消息, data=[helloworld, 123, true]}
+    }
 
 }
