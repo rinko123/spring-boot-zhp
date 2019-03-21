54d492439e70fb7d90fefce1da069d04c677e597
Rinko <qw85123163@163.com>
2019-03-21 15:22:46 +0800

    04 Task - 异步任务、标记方法为异步方法（@EnableAsync、@Async）

--- springboot-04-task/src/main/java/com/atguigu/task/Springboot04TaskApplication.java
+++ springboot-04-task/src/main/java/com/atguigu/task/Springboot04TaskApplication.java
@@ -2,7 +2,9 @@ package com.atguigu.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync  //开启异步注解功能
 @SpringBootApplication
 public class Springboot04TaskApplication {
 
