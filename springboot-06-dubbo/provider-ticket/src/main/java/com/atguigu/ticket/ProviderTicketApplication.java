bb9afd5c4c547b22b0e731084187bb0216375b44
Rinko <qw85123163@163.com>
2019-04-10 16:38:52 +0800

    06 Dubbo - provider - 配置dubbo的扫描包和注册中心地址、使用@Service发布服务

--- springboot-06-dubbo/provider-ticket/src/main/java/com/atguigu/ticket/ProviderTicketApplication.java
+++ springboot-06-dubbo/provider-ticket/src/main/java/com/atguigu/ticket/ProviderTicketApplication.java
@@ -3,6 +3,12 @@ package com.atguigu.ticket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1、将服务提供者注册到注册中心
+ * 	    1、引入dubbo和zkclient相关依赖
+ * 	    2、编写业务service
+ *   	3、配置dubbo的扫描包和注册中心地址、使用@Service发布服务
+ */
 @SpringBootApplication
 public class ProviderTicketApplication {
 
--- springboot-06-dubbo/provider-ticket/src/main/java/com/atguigu/ticket/service/TicketServiceImpl.java
+++ springboot-06-dubbo/provider-ticket/src/main/java/com/atguigu/ticket/service/TicketServiceImpl.java
@@ -1,8 +1,15 @@
 package com.atguigu.ticket.service;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import org.springframework.stereotype.Component;
 
 @Component
+@Service(
+        version = "${provider.service.version}",
+        application = "${dubbo.application.id}",
+        protocol = "${dubbo.protocol.id}",
+        registry = "${dubbo.registry.id}"
+)
 public class TicketServiceImpl implements TicketService {
 
     @Override
